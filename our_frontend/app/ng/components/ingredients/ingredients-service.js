c6388df06442c4f19e0fc1ca8a5be555cc61952b
Florian Noack <noack@fortiss.org>
2016-06-28 10:39:45 +0200

    added components for create new recipe

---
+++ our_frontend/app/ng/components/ingredients/ingredients-service.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Florian Noack on 27.06.2016.
+ */
+
+'use strict';
+
+angular.module('myApp.recipes')
+
+    .factory('Ingredient', function($resource) {
+        return $resource('http://localhost:3000/api/ingredient/:ingredient_id', {ingredient_id: '@_id'});
+    });
