561b1df7e9dd2dc0870d9326396c5aebb502ca6c
Praveer Rai <raipraveer@gmail.com>
2016-07-03 15:46:50 +0200

    adding default values

--- our_backend/order/orderSchema.js
+++ our_backend/order/orderSchema.js
@@ -13,10 +13,19 @@ var Order = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
-    marketId: String,
-    paymentMethod: String,
+    marketId: {
+        type: String,
+        default: 'market123'
+    },
+    paymentMethod: {
+        type: String,
+        default: 'Paypal'
+    },
     shipAddress: String,
-    orderStatus: String,
+    orderStatus: {
+        type: String,
+        default: 'open'
+    },
     extraInfo: String
 });
 
